9c6d3912f3788c453d755318dde5f84a052ae3ce
oscarprdev <vlcoscar8@gmail.com>
2024-01-29 07:58:38 +0100

    Fix join sql query

--- backend/pkg/features/users/infra/sql/queries/join_user_url.sql
+++ backend/pkg/features/users/infra/sql/queries/join_user_url.sql
@@ -1,4 +1,4 @@
-SELECT urls.id, urls.short_url, urls.original_url, urls.title, urls.expires_at
+SELECT urls.id, urls.original_url, urls.short_url, urls.title, urls.expires_at
 FROM users
 INNER JOIN url_user ON users.id = url_user.user_id
 INNER JOIN urls ON url_user.url_id = urls.id
